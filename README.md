cb95f57f70fdaaddf22da6737cd34e76c6d4f3f8
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-12-07 18:49:08 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,130 @@
+## About
+
+This ChatGPT clone allows you to ask a chatbot anything about Pokemon, from current Pokemon events, to abilities. You can even ask it to show you images of the Pokemon.
+
+## Architecture
+
+### Conversation workflow
+
+- User goes to React website that's hosted on AWS Amplify. Asks a question about Pokemon.
+- Question goes to NodeJs server that's hosted in AWS Elastic Beanstalk.
+- NodeJs saves messages to a Postgres AWS RDS database.
+- NodeJs also sends question to OpenAI API.
+- OpenAI API does inference and either sends back the answer, or suggests what tools and arguments to use to NodeJs,
+- NodeJs always saves the messages to Postgres.
+- If OpenAI answered directly (finish_reason:stop), it is sent to React.
+- If it's a tool suggestion, NodeJs calls the tool with the given arguments, either the Tavily web search tool, or the PokeAPI tool,
+- Again NodeJs saves to Postgres the results of the tool call,
+- NodeJs either sends the tool call result to OpenAI to get a response to the search results for Tavily, OR a placeholder response is created for PokeAPI,
+- NodeJs again saves the response to Postgres,
+- Response is sent to React.
+
+## The live site is at https://master.d25jr6vo3627gh.amplifyapp.com/
+
+## Instructions to run app locally
+
+- Have git installed. https://gitforwindows.org/
+- Have NodeJs installed. https://nodejs.org/
+- For backend:
+
+  - In a command prompt, run
+    - `git clone https://github.com/andrewcbuensalida/typescript-node-project.git`
+  - `cd typescript-node-project`
+  - Install node_modules
+    - `npm ci`
+  - Get an OPENAI api key, Tavily API key, then fill in .env file with it
+  - `npm run local`
+
+- For the database,
+
+  - Install Docker desktop. https://www.docker.com/products/docker-desktop and make sure it's running
+  - to use my Postgres container that has empty tables initialized:
+    - Pull then run in one step with
+      - `docker run -d --name pokemon-postgres-container -e POSTGRES_USER=yourusername -e POSTGRES_PASSWORD=yourpassword -p 5433:5432 andrewcbuensalida/pokemon-postgres-image:latest`
+  - Alternatively, to use a Postgres container that you have to manually initialize the tables
+
+    - Have a generic PostreSQL container running:
+      - `docker run --name pokemon-postgres-container -e POSTGRES_PASSWORD=yourpassword -e POSTGRES_USER=yourusername -p 5433:5432 -d postgres`
+      - --name is container name. -p is port mapping. --rm will delete container when done.
+    - Install ts-node
+      - `npm i ts-node --global`
+    - Create tables in the postgres container. cd to init-db.ts. Make sure CREATE DATABASE is commented out, then run
+      - `ts-node init-db.ts`
+      - You can do this too if you want to quickly reset the database
+
+  - To check if tables are successfully created
+    - In postgres container, connect to database with
+      - `psql -U yourusername -d pokemon_chatbot_db -p 5432`
+    - List databases
+      - `\l`
+    - to list tables
+      - `\dt`
+    - If you weren't connected to a database
+      - `\c <databaseName>`
+    - Check inside the table
+      - `SELECT * FROM messages;`
+    - disconnect from psql
+      - `\q`
+
+- For the front-end:
+  - In command prompt
+    - `git clone https://github.com/andrewcbuensalida/react-chatgpt-clone-meta`
+    - `cd react-chatgpt-clone-meta`
+    - `npm ci`
+    - `npm run start`
+    - Go to http://localhost:3000/
+
+## To create a postgres docker image that has empty tables already initialized
+
+- create schema.sql. Make sure it has CREATE DATABASE uncomented
+- create a Dockerfile in the same folder.
+- Build the image
+  - `docker build -t pokemon-postgres-image .`
+  - This will copy schema.sql into the container folder that postgres runs automatically on run
+- Run the container
+  `docker run -d --name pokemon-postgres-container -e POSTGRES_PASSWORD=yourpassword -e POSTGRES_USER=yourusername -p 5433:5432 pokemon-postgres-image`
+
+## To push postgres image to dockerhub so others can run it
+
+- Create a dockerhub account
+- In command prompt
+  - `docker login`
+- Tag the image
+  - `docker tag pokemon-postgres-image andrewcbuensalida/pokemon-postgres-image:latest`
+- `docker push andrewcbuensalida/pokemon-postgres-image:latest`
+- OR you can use the docker desktop interface to push
+
+## To deploy backend to AWS ECS
+- Build the image. Go to root of this folder, run
+  - `docker build -t pokemon-node-image .`
+  - This uses the Dockerfile, copies package-lock.json, installs node_modules in container, converts ts to js.
+- Test the container by running it. Make sure IS_IN_CONTAINER is true if hitting a local db
+  - `docker run --rm --name pokemon-node-container --env-file .env -p 5000:5000 pokemon-node-image`
+- Create an empty ECR repo in aws console. Name the repo the same as the image name. Look at push commands:
+  - Login, In powershell (must have AWS Tools for Powershell installed),
+    `(Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 597043972440.dkr.ecr.us-west-1.amazonaws.com`
+  - Tag the image. This duplicates the image with name 5970....
+    `docker tag pokemon-node-image:latest 597043972440.dkr.ecr.us-west-1.amazonaws.com/pokemon-node-image:latest`
+  - Push
+    `docker push 597043972440.dkr.ecr.us-west-1.amazonaws.com/pokemon-node-image:latest`
+- In ECS
+  - Create cluster
+  - Create target group in ec2
+  - Create a task definition. 
+    - Task Role and Task Execution Role should be ecsTaskExecutionRole. 
+    - There was no ecsTaskExecutionRole option in the dropdown the first time, but the second time there was. 
+    - Need port 5000 mapping, and maybe 80 for safe measure. 
+    - Individually add environment variables, safer than pointing to an s3. Copy prod.env s3 arn to here.
+    - command for health check should be
+      - `CMD-SHELL, curl -f http://localhost/healthCheck || exit 1` 
+  - Create a service (in cluster section) with application load balancer. Select task family from previous step. This will run a cloudformation stack.
+
+## TODO
+
+- better instructions
+- front-end should send chat_id
+- auth
+- Deploy be elastic Beanstalk
+- design diagram
+- add to portfolio
+- dry
---
+++ src/App.tsx
@@ -0,0 +1,345 @@
+import {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useLayoutEffect,
+} from 'react'
+import { BiPlus, BiUser, BiSend, BiSolidUserCircle } from 'react-icons/bi'
+import { MdOutlineArrowLeft, MdOutlineArrowRight } from 'react-icons/md'
+
+const serverUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://pokemon4-0770c86f5a12.herokuapp.com' // TODO
+
+const fetchOptions = {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer andrewcbuensalida',
+  },
+}
+
+function App() {
+  const [text, setText] = useState<any>('')
+  const [allMessages, setAllMessages] = useState<any>([])
+  const [currentTitle, setCurrentTitle] = useState<any>(null)
+  const [isResponseLoading, setIsResponseLoading] = useState<any>(false)
+  const [errorText, setErrorText] = useState<any>('')
+  const [isShowSidebar, setIsShowSidebar] = useState<any>(false)
+  const scrollToLastItem = useRef<any>(null)
+  const inputRef = useRef<any>(null)
+
+  const currentMessages = allMessages.filter(
+    (msg: any) => msg.title === currentTitle
+  )
+  const messagesToday = allMessages.filter(
+    (msg: any) =>
+      new Date(msg.createdAt).toDateString() === new Date().toDateString()
+  )
+  const messagesTodayUniqueTitles = Array.from(
+    new Set(messagesToday.map((msg: any) => msg.title))
+  ).reverse()
+
+  const messagesNotTodayUniqueTitles = Array.from(
+    new Set(
+      allMessages
+        .map((msg: any) => msg.title)
+        .filter((title: any) => !messagesTodayUniqueTitles.includes(title))
+    )
+  ).reverse()
+
+  const createNewChat = () => {
+    setText('')
+    setCurrentTitle(null)
+    inputRef.current?.focus()
+  }
+
+  const handleTitleClick = (uniqueTitle: any) => {
+    setCurrentTitle(uniqueTitle)
+    setText('')
+    inputRef.current?.focus()
+  }
+
+  const toggleSidebar = useCallback(() => {
+    setIsShowSidebar((prev: any) => !prev)
+  }, [])
+
+  const submitHandler = async (e: any) => {
+    e.preventDefault()
+
+    if (!text) return
+
+    // temporarily add the message to the list. This will be replaced by the actual response since setAllMessages below uses an outdated allMessages state.
+    setAllMessages([
+      ...allMessages,
+      {
+        id: Math.random().toString(36).substr(2, 9),
+        title: currentTitle || text,
+        role: 'user',
+        content: text,
+        createdAt: new Date().toISOString(),
+        userId: '1',
+      },
+    ])
+
+    setCurrentTitle(currentTitle || text)
+
+    setTimeout(() => {
+      scrollToLastItem.current?.lastElementChild?.scrollIntoView({
+        behavior: 'smooth',
+      })
+    }, 1)
+    setIsResponseLoading(true)
+    setErrorText('')
+
+    try {
+      const response = await fetch(`${serverUrl}/api/completions`, {
+        ...fetchOptions,
+        method: 'POST',
+        body: JSON.stringify({
+          message: text,
+          title: currentTitle,
+        }),
+      })
+
+      if (response.status === 429) {
+        return setErrorText('Too many requests, please try again later.')
+      }
+
+      const data = await response.json()
+
+      if (data.error) {
+        setErrorText(data.error.message)
+        setText('')
+      } else {
+        setErrorText('')
+      }
+
+      if (!data.error) {
+        setErrorText('')
+        setTimeout(() => {
+          scrollToLastItem.current?.lastElementChild?.scrollIntoView({
+            behavior: 'smooth',
+          })
+        }, 1)
+        setTimeout(() => {
+          setText('')
+        }, 2)
+        setAllMessages([...allMessages, ...data.newMessages])
+        setCurrentTitle(data.newMessages[0].title)
+      }
+    } catch (e: any) {
+      setErrorText('Error sending message. Please try again later.')
+      console.error(e)
+    } finally {
+      setIsResponseLoading(false)
+    }
+  }
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setIsShowSidebar(window.innerWidth <= 640)
+    }
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  useEffect(() => {
+    async function getAllMessages() {
+      try {
+        console.log(`*Fetching previous messages...`)
+        const response = await fetch(`${serverUrl}/api/messages`, fetchOptions)
+        const data = await response.json()
+        setAllMessages(data)
+      } catch (error) {
+        console.error('Error fetching previous messages:', error)
+      }
+    }
+
+    getAllMessages()
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <section className={`sidebar ${isShowSidebar ? 'open' : ''}`}>
+          <div className="sidebar-header" onClick={createNewChat} role="button">
+            <BiPlus size={20} />
+            <button>New Chat</button>
+          </div>
+          <div className="sidebar-history">
+            {messagesTodayUniqueTitles.length > 0 && (
+              <>
+                <p>Today</p>
+                <ul>
+                  {messagesTodayUniqueTitles?.map((uniqueTitle: any) => {
+                    return (
+                      <li
+                        key={uniqueTitle}
+                        onClick={() => handleTitleClick(uniqueTitle)}
+                        className={`App_Unique_Title ${
+                          uniqueTitle === currentTitle ? 'active' : ''
+                        }`}
+                      >
+                        {uniqueTitle}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </>
+            )}
+            {messagesNotTodayUniqueTitles.length > 0 && (
+              <>
+                <p>Previous</p>
+                <ul>
+                  {messagesNotTodayUniqueTitles?.map(
+                    (uniqueTitle: any, idx) => {
+                      return (
+                        <li
+                          key={idx}
+                          onClick={() => handleTitleClick(uniqueTitle)}
+                          className={`App_Unique_Title ${
+                            uniqueTitle === currentTitle ? 'active' : ''
+                          }`}
+                        >
+                          {uniqueTitle}
+                        </li>
+                      )
+                    }
+                  )}
+                </ul>
+              </>
+            )}
+          </div>
+          <div className="sidebar-info">
+            <div className="sidebar-info-upgrade">
+              <BiUser size={20} />
+              <p>Upgrade plan</p>
+            </div>
+            <div className="sidebar-info-user">
+              <BiSolidUserCircle size={20} />
+              <p>User</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="main">
+          {!currentTitle && (
+            <div className="empty-chat-container">
+              <img src="pokeball.png" width={45} height={45} alt="ChatGPT" />
+              <h1>Pokemon Assistant</h1>
+              <h3>
+                Say 'show me Pikachu'
+                <br />
+                Say 'picture of 151st pokemon'
+                <br />
+                Say 'Pokemon events in San Diego'
+                <br />
+                Say 'What are Charizards attacks?'
+                <br />
+                or ask me anything about Pokemon
+              </h3>
+            </div>
+          )}
+
+          {isShowSidebar ? (
+            <MdOutlineArrowRight
+              className="burger"
+              size={28.8}
+              onClick={toggleSidebar}
+            />
+          ) : (
+            <MdOutlineArrowLeft
+              className="burger"
+              size={28.8}
+              onClick={toggleSidebar}
+            />
+          )}
+          <div className="main-header">
+            <ul>
+              {currentMessages?.map((chatMsg: any) => {
+                const isUser = chatMsg.role === 'user'
+                return (
+                  <li key={chatMsg.id} ref={scrollToLastItem}>
+                    {isUser ? (
+                      <div>
+                        <BiSolidUserCircle size={28.8} />
+                      </div>
+                    ) : (
+                      <img
+                        className="avatar"
+                        src="pokeball.png"
+                        alt="ChatGPT"
+                      />
+                    )}
+                    {isUser ? (
+                      <div>
+                        <p className="role-title">You</p>
+                        <p>{chatMsg.content}</p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p className="role-title">Pokemon Assistant</p>
+                        <p>
+                          {chatMsg.toolName === 'getPokemonImageByName' &&
+                          !chatMsg.errorMessage ? (
+                            <img
+                              src={JSON.parse(chatMsg.content).pokemonImage}
+                              alt="Pokemon"
+                              width={300}
+                              height={300}
+                            />
+                          ) : (
+                            chatMsg.content
+                              ?.split('\n')
+                              .map((line: any, index: any) => (
+                                <>
+                                  {line}
+                                  <br />
+                                </>
+                              ))
+                          )}
+                        </p>
+                      </div>
+                    )}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="main-bottom">
+            {errorText && <p className="errorText">{errorText}</p>}
+            <form className="form-container" onSubmit={submitHandler}>
+              <input
+                ref={inputRef}
+                type="text"
+                placeholder="Send a message."
+                spellCheck="false"
+                value={isResponseLoading ? 'Processing...' : text}
+                onChange={(e) => setText(e.target.value)}
+                readOnly={isResponseLoading}
+              />
+              {!isResponseLoading && (
+                <button type="submit">
+                  <BiSend size={20} />
+                </button>
+              )}
+            </form>
+            <p>
+              ChatGPT can make mistakes. Consider checking important
+              information.
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
